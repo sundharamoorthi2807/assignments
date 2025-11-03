67fea7d2005451cf5cd74c9d2092a22d9178fee1
Sundharamoorthi <yukesh510@gmail.com>
2025-11-04 00:34:16 +0530

    Assignment 1 - Data types

---
+++ src/assignments/Assignment1.java
@@ -0,0 +1,74 @@
+package assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		double celsius = 25.5;
+		System.out.println("Temperature of a city in degrees Celsius: "+celsius);
+		
+		boolean yes = true;
+		boolean no = false;
+		System.out.println("Whether a customer has placed an order: "+yes+"or"+no);
+		
+		
+		String phonenumber = "123-456-7890";
+		System.out.println("Person's phone number: "+phonenumber);
+
+		float amount = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account: "+amount);
+		
+		String email = "john.doe@example.com";
+		System.out.println("Person's email address: "+email);
+	
+		float latitute = 37.7749f;
+		float longitute = -122.4194f;
+		System.out.println("Coordinates of a location (latitude, longitude): "+latitute +","  +longitute);
+		
+		boolean married = true;
+		boolean notmarried = false;
+		System.out.println("Person's marital status: "+married+  "or" +notmarried);
+		String occupation = "Software Engineer";
+		System.out.println("Person's occupation: "+occupation);
+		
+		String colour = "Blue";
+		System.out.println("Person's favourite colour: "+colour);
+		
+		int year = 2023;
+		System.out.println("Current year:"+year);
+		
+		int follower = 1000000;
+		System.out.println("Number of followers on a social media platform: "+follower);
+		
+		float rating = 7.5f;
+		System.out.println("Rating of a movie: "+rating);
+		
+		char blood = 'A';
+		System.out.println("Person's blood type: "+blood);
+		
+		String book = "To Kill a Mockingbird";
+		System.out.println("14.Title of a book: "+book);
+		
+		int employees = 500;
+		System.out.println("Number of employees in a company: "+employees);
+		
+		String time = "2.30 PM";
+		System.out.println("Time of an event: "+time);
+		
+		
+		String country = "United States";
+		System.out.println("Name of a country: "+country);
+		
+		String color = "Brown";
+		System.out.println("Person's eye colour: "+color);
+		
+	
+		String place = "New York City";
+		System.out.println("Person's birthplace: "+place);
+
+		
+		float distance = 200.5f;
+		System.out.println("Distance between two cities: "+distance);
+		
+	}
+
+}
--- src/language/basics/HelloWorld.java
+++ src/language/basics/HelloWorld.java
@@ -3,7 +3,10 @@ package language.basics;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+				System.out.println("Hello, world!");
+		System.out.println("Sundhar Automation tester");
+		System.out.println("Sundhar Automation tester");
+
 		
 
 	}
